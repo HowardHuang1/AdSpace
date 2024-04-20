c36f8f46c70903250f9d7355981f32d930ec6e0b
Howard Huang <howardhuang2004@gmail.com>
2024-04-20 03:50:27 -0700

    create navbar

--- addspace/frontend/src/App.js
+++ addspace/frontend/src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Router>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
